58addcb4944e464961ca1b8de5d757a2196699d4
Felipe Alves de Jesus <felipe.jesus@ext.vallourec.com>
2023-03-26 23:30:29 -0300

    creating boilerplate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+  site: "https://www.my-site.dev",
+  integrations: [vue(), tailwind(), react()],
+  build: {
+    // Example: Generate `page.html` instead of `page/index.html` during build.
+    format: "directory",
+  },
+});
